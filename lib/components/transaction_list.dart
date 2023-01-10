b1bdb0a065110358ae4bee93a8bdc35750d7aa9c
llucasmota <lucas.o.mota@gmail.com>
2023-01-10 08:56:13 -0300

    feat(transaction_list): implementando key para que a aplicação não tenha problemas na lógica da listagem

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,13 +1,11 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'package:expenses/components/transaction_item.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final void Function(String) onRemove;
-  TransactionList(this.transactions, this.onRemove);
+  const TransactionList(this.transactions, this.onRemove, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +24,7 @@ class TransactionList extends StatelessWidget {
                 ),
                 SizedBox(height: constraints.maxHeight * 0.05),
                 Container(
-                  padding: EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
                   height: constraints.maxHeight * 0.6,
                   child: Image.asset(
                     'assets/images/waiting.png',
@@ -40,39 +38,10 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final transaction = transactions[index];
-              return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                          child: Text('R\$${transaction.value}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      transaction.title,
-                      style: Theme.of(context).textTheme.titleSmall,
-                    ),
-                    subtitle:
-                        Text(DateFormat('d MMM y').format(transaction.date)),
-                    trailing: MediaQuery.of(context).size.width > 480
-                        ? TextButton.icon(
-                            onPressed: () => onRemove(transaction.id),
-                            icon: Icon(Icons.delete),
-                            label: Text('Excluir'),
-                            style: TextButton.styleFrom(
-                                foregroundColor: Theme.of(context).errorColor),
-                          )
-                        : IconButton(
-                            icon: Icon(Icons.delete),
-                            color: Theme.of(context).errorColor,
-                            onPressed: () => onRemove(transaction.id),
-                          ),
-                  ));
+              return TransactionItem(
+                  key: GlobalObjectKey(transaction.id),
+                  transaction: transaction,
+                  onRemove: onRemove);
             },
           );
   }
