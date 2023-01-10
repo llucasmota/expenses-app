7b533c4039fc41ebfe5939fde18dfbb730221c46
llucasmota <lucas.o.mota@gmail.com>
2023-01-10 08:54:32 -0300

    feat(chart_bar): adicionando key

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class ChartBar extends StatelessWidget {
   final String label;
   final double value;
   final double percentage;
 
-  ChartBar(
-      {required this.label, required this.value, required this.percentage});
+  const ChartBar(
+      {super.key,
+      required this.label,
+      required this.value,
+      required this.percentage});
   @override
   Widget build(BuildContext context) {
     print('build() ChartBar');
